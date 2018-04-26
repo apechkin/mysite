e1dea7801bd7e55e82760337ec561b4f0dc144b3
Anatoliy Pechkin <anatmozg@mail.ru>
2018-04-26 23:39:19 +0300

    initial commit

---
+++ src/components/Layout/header.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div class="header">
+    <ul v-for="(element, index) in header"  :key="index">
+      <li>{{element.toUpperCase()}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+export default {
+  name: 'Header',
+  data () {
+    return {
+      header: [ 'home', 'about', 'experience', 'education', 'skills', 'contact' ]
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.header {
+  display: flex;
+  width: 1176px;
+  margin-left: auto;
+  margin-right: auto;
+  justify-content: space-around;
+}
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div class="conteiner">
+      <div class="content">
+        <header-layout />
+        <router-view/>
+      </div>
+    </div>
+    <div class="side">
+      Side
+    </div>
+    <div class="footer">
+      Footer
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderLayout from './Layout/header.vue'
+export default {
+  name: 'App',
+  components: {
+    HeaderLayout
+  }
+}
+</script>
+
+<style lang="less">
+@import './assets/global.less';
+</style>
